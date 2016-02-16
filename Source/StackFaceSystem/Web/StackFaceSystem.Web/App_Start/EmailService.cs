dece048028439b997695e393b4b30a7ced8ca9b4
EmilMitev <Mitev.e93@gmail.com>
2016-02-16 09:47:28 +0200

    Extracted classes in IdentityConfig.cs to separate files

---
+++ Source/StackFaceSystem/Web/StackFaceSystem.Web/App_Start/EmailService.cs
@@ -0,0 +1,21 @@
+﻿namespace StackFaceSystem.Web
+{
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin;
+    using Microsoft.Owin.Security;
+    using Data.Models;
+    using Data;
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ Source/StackFaceSystem/Web/StackFaceSystem.Web/App_Start/SmsService.cs
@@ -0,0 +1,22 @@
+﻿namespace StackFaceSystem.Web
+{
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin;
+    using Microsoft.Owin.Security;
+    using Data.Models;
+    using Data;
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your SMS service here to send a text message.
+            return Task.FromResult(0);
+        }
+    }
+}
