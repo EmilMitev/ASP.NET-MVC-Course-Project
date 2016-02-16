455524d460647c2950a5b233fc49f0512bfa9119
EmilMitev <Mitev.e93@gmail.com>
2016-02-16 09:01:19 +0200

    Moved classes in AccountViewModels.cs and ManageViewModels.cs files to respective namespaces. Extracted classes in AccountViewModels.cs to separate files Extracted classes in ManageViewModels.cs to separate files

--- Source/StackFaceSystem/Web/StackFaceSystem.Web/Controllers/AccountController.cs
+++ Source/StackFaceSystem/Web/StackFaceSystem.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using StackFaceSystem.Web.Models;
+using StackFaceSystem.Web.ViewModels.Account;
 
 namespace StackFaceSystem.Web.Controllers
 {
