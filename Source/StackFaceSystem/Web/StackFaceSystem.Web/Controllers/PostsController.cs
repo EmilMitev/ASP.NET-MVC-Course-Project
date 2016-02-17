33818b6531889b6e66fb217c748f180e5cd3f836
EmilMitev <Mitev.e93@gmail.com>
2016-02-17 18:37:33 +0200

    Added posts page design

---
+++ Source/StackFaceSystem/Web/StackFaceSystem.Web/Controllers/PostsController.cs
@@ -0,0 +1,15 @@
+﻿namespace StackFaceSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class PostsController : BaseController
+    {
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+
+    }
+}
