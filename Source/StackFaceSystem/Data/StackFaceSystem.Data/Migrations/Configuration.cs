fc8c194f825b5ccb4785d4323891d56a3b42cfbe
EmilMitev <Mitev.e93@gmail.com>
2016-02-24 23:38:20 +0200

    Add roles

--- Source/StackFaceSystem/Data/StackFaceSystem.Data/Migrations/Configuration.cs
+++ Source/StackFaceSystem/Data/StackFaceSystem.Data/Migrations/Configuration.cs
@@ -14,5 +14,25 @@
             this.AutomaticMigrationsEnabled = true;
             this.AutomaticMigrationDataLossAllowed = true;
         }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            if (!context.Roles.Any())
+            {
+                // For roles
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                IdentityRole role;
+                role = new IdentityRole { Name = GlobalConstants.AdministratorRoleName };
+                roleManager.Create(role);
+
+                role = new IdentityRole { Name = GlobalConstants.ModeratorRoleName };
+                roleManager.Create(role);
+
+                role = new IdentityRole { Name = GlobalConstants.UserRoleName };
+                roleManager.Create(role);
+            }
+        }
     }
 }
